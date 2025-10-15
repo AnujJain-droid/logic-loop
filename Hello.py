b4e380e0d178f308b3f23891c47805865b3c0fb9
AnujJain-droid <anujjain1108@gmail.com>
2025-10-15 13:25:37 +0530

    added table of 2

--- Hello.py
+++ Hello.py
@@ -1,1 +1,12 @@
-print("anuj")
+#!/usr/bin/env python3
+"""Print multiplication table of 2 (1 through 10)."""
+
+def print_table(n: int = 2, upto: int = 10) -> None:
+	"""Print multiplication table for n from 1 to upto (inclusive)."""
+	for i in range(1, upto + 1):
+		print(f"{i} x {n} = {i * n}")
+
+
+if __name__ == "__main__":
+	print_table(2, 10)
+
